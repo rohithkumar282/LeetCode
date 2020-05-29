73bf615758b5dd0efdb53bbf408f923675667b3c
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-05-28 21:26:50 -0700

    #1

---
+++ LeetCode#1.py
@@ -0,0 +1,18 @@
+/* Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/two-sum/
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        k=0
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if target==nums[i]+nums[j]:
+                    return [i,j]
+                    k=1
+                    break
+            if k==1:
+                break
