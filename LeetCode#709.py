81f9dbff68bf61b4c5ee853e181f7dafeb2548c6
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-05-28 21:47:03 -0700

    #709

---
+++ LeetCode#709.py
@@ -0,0 +1,10 @@
+'''
+   Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/to-lower-case/
+'''
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        str=str.lower()
+        return str
