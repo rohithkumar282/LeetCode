ef485064cce1564c3a240bc70a897c88d530bfa1
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-06-04 23:08:21 -0700

    #268

---
+++ LeetCode268.py
@@ -0,0 +1,11 @@
+'''
+   Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/missing-number/
+'''
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n+1):
+            if i not in nums:
+                return i
