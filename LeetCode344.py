0eec653504d3881d951f66f8d2233f264ab831cd
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-06-04 23:18:48 -0700

    #344

---
+++ LeetCode344.py
@@ -0,0 +1,11 @@
+'''
+   Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/reverse-string/
+'''
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
