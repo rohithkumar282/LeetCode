2ea08525d54aa30d3824cc0b9d9ec358454f354f
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-06-15 17:52:31 -0700

    #1108

---
+++ LeetCode1108.py
@@ -0,0 +1,8 @@
+'''
+   Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/shuffle-the-array/
+'''
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".","[.]")
