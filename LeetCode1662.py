f26d7d2b6e4090df0133f71d49dddb0a02b0698f
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2020-11-23 12:11:02 -0700

    '1662'

---
+++ LeetCode1662.py
@@ -0,0 +1,8 @@
+'''
+   Author: Rohith Kumar Punithavel
+   Email: rohithkumar@asu.edu
+   Problem Statement: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+'''
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1)==''.join(word2)
